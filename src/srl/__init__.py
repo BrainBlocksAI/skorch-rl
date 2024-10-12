33ae162741b708cfa37afd0aecd4fc6e0af0d6a6
georgedouzas <gdouzas@icloud.com>
2024-10-12 14:23:18 +0300

    chore: Remove support for Python 3.10 version

--- src/srl/__init__.py
+++ src/srl/__init__.py
@@ -1,5 +1,6 @@
-"""A Reinforcement Learning library, that implements a variety of
-algorithms.
+"""A Reinforcement Learning library, that implements a variety of algorithms.
+
+It is designed to extend skorch while maintaining the simplicity of scikit-learn's API.
 """
 
 from __future__ import annotations
